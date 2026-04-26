4006a685b557de556e90b61d8060ce5cc3f3fd6e
chicaizadiego55-tech <chicaizadiego55@gmail.com>
2026-04-26 05:10:32 -0500

    taller finalizado

--- multiplicar.js
+++ multiplicar.js
@@ -1,15 +1,24 @@
-function generarTablas(){
-    let contenedor= document.getElementById("divContenedor");
-    
-    let contenido= "";
-   
-    for(let i = 1; i <= 12; i++){
-        contenido += "<div class='fila'><span>3 x " + i + " =</span><span>" + (3 * i) + "</span></div>";
-    }
-    for(let i = 1; i <= 12; i++){
-        contenido += "<div class='fila'><span>5 x " + i + " =</span><span>" + (5 * i) + "</span></div>";
-        
-    }
-        contenedor. innerHTML= contenido;
-        
+function generarTablas() {
+  let numero = parseInt(document.getElementById("txtNumero").value);
+  let contenedor = document.getElementById("divContenedor");
+  let contenido = "";
+
+  if (isNaN(numero)) {
+    contenedor.innerHTML = "<div class='error'>⚠️ Por favor ingresa un número</div>";
+    return;
+  }
+  contenido += "<h1>🔢 Tabla del " + numero + "</h1>";
+  contenido += "<p class='subtitulo'>¡Aquí está tu tabla!</p>";
+  contenido += "<div class='divisor'></div>";
+  contenido += "<div class='tabla'>";
+
+  for (let i = 1; i <= 12; i++) {
+    contenido += "<div class='fila'><span>" + numero + " x " + i + " =</span><span>" + (numero * i) + "</span></div>";
+  }
+
+  contenido += "</div>";
+  contenido += "<br><div class='estrellas'>⭐ 🌙 ⭐</div>";
+
+  contenedor.innerHTML = contenido;
+
 }
